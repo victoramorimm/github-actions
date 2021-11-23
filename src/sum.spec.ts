3655616f9a8b9d0439d9c69505b0530f9fe9c39b
Victor Amorim <victorvmrgamer@gmail.com>
2021-11-23 08:02:31 -0300

    feat: add second test

--- src/sum.spec.ts
+++ src/sum.spec.ts
@@ -6,3 +6,12 @@ test("adds 1 + 2 to equal 3", () => {
 
   expect(sum).toBe(3);
 });
+
+test("adds 4 + 2 is not equal to 3", () => {
+  const firstNumber = 4;
+  const secondNumber = 2;
+
+  const sum = firstNumber + secondNumber;
+
+  expect(sum).not.toBe(3);
+});
